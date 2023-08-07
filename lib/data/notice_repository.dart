addbf1fa28c99ebfcf9df145bf8f750337d4bb04
qweehd <qweehd@naver.com>
2023-08-07 23:14:09 +0700

    [데이터연결]공지사항 : 레파지토리 및 뷰모델 생성

---
+++ lib/data/notice_repository.dart
@@ -0,0 +1,23 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+
+import '../models/ModelProvider.dart';
+
+class NoticeRepository {
+  Future<List<PublicNotice?>> queryListItems() async {
+    try {
+      final request = ModelQueries.list(PublicNotice.classType);
+      final response = await Amplify.API.query(request: request).response;
+
+      final noticeList = response.data?.items;
+      if (noticeList == null) {
+        safePrint('데이터가 없습니다. ${response.errors}');
+        return [];
+      }
+      return noticeList;
+    } on ApiException catch (e) {
+      safePrint('데이터를 가져오는데 실패했습니다. ${e.message}');
+      return [];
+    }
+  }
+}
---
+++ lib/viewmodels/notice_viewmodel.dart
@@ -0,0 +1,26 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/cupertino.dart';
+import '../data/notice_repository.dart';
+import '../models/ModelProvider.dart';
+
+class NoticeViewModel extends ChangeNotifier {
+  final NoticeRepository _noticeRepository;
+
+  NoticeViewModel({required NoticeRepository noticeRepository})
+      : _noticeRepository = noticeRepository;
+
+  List<PublicNotice> _noticeItems = [];
+
+  List<PublicNotice> get noticeItems => _noticeItems;
+
+  bool noticeLoading = false;
+
+  Future<void> queryPostItems() async {
+    noticeLoading = true;
+    Future.microtask(() => notifyListeners());
+    final noticeList = await _noticeRepository.queryListItems();
+    _noticeItems = noticeList.map((e) => e as PublicNotice).toList();
+    noticeLoading = false;
+    Future.microtask(() => notifyListeners());
+  }
+}
