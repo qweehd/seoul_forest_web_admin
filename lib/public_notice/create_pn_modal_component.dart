bfcdcd7f85479992a95efc2bc4495396d253f4d3
sprisao <whdtmek@gmail.com>
2023-08-25 20:07:30 +0900

    Refactor public notice modal component
    Refactored public notice model component to improve code maintainability and enhance user experience. Action buttons ('완료' button) are removed to streamline user navigation. The redundant repeated parts of the code have been extracted into two separate functions: 'generatePublicNoticeForCountries' and 'generatePublicNoticeForCities', which improves code readability and maintainability. Additionally, the entire body of the layout has been wrapped into SingleChildScrollView to handle overflow and provide a better user experience.

--- lib/public_notice/create_pn_modal_component.dart
+++ lib/public_notice/create_pn_modal_component.dart
@@ -118,107 +118,50 @@ class _CreatePublicNoticeModalState extends State<CreatePublicNoticeModal> {
         },
         icon: const Icon(Icons.arrow_back),
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('완료', style: TextStyle(color: Colors.white)),
-        ),
-      ],
     );
   }
 
   Widget _createBody() {
-    return Column(
-      children: [
-        const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('공지사항 범위'),
-            const SizedBox(width: 10),
-            CupertinoSwitch(
-                value: nationalScope,
-                onChanged: (value) {
-                  setState(() {
-                    nationalScope = value;
-                  });
-                }),
-            const SizedBox(width: 10),
-            Text(nationalScope ? '나라별' : '도시별'),
-          ],
-        ),
-        const Center(
-          child: Text('공지할 나라', style: TextStyle(fontSize: 20)),
-        ),
-        const SizedBox(height: 20),
-        _countries.isEmpty
-            ? const SizedBox()
-            : Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: _countries
-                    .map((country) => GestureDetector(
-                          onTap: () {
-                            onSelectedCountry(country);
-                          },
-                          child: Container(
-                              constraints: const BoxConstraints(
-                                  minWidth: 80, minHeight: 80),
-                              decoration: BoxDecoration(
-                                  border: _selectedCountries.contains(country)
-                                      ? Border.all(
-                                          color: Colors.green, width: 5)
-                                      : Border.all(color: Colors.grey),
-                                  borderRadius: BorderRadius.circular(10)),
-                              padding: const EdgeInsets.all(10),
-                              margin: const EdgeInsets.only(right: 10),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    country.flagEmoji,
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                  Text(
-                                    country.name,
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                ],
-                              )),
-                        ))
-                    .toList(),
-              ),
-        const SizedBox(height: 20),
-        if (!nationalScope)
-          for (var country in _selectedCountries)
-            Column(
-              children: [
-                const SizedBox(height: 20),
-                Text('${country.name}의 도시',
-                    style: const TextStyle(fontSize: 20)),
-                const SizedBox(height: 20),
-                Row(
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('공지사항 범위'),
+              const SizedBox(width: 10),
+              CupertinoSwitch(
+                  value: nationalScope,
+                  onChanged: (value) {
+                    setState(() {
+                      nationalScope = value;
+                    });
+                  }),
+              const SizedBox(width: 10),
+              Text(nationalScope ? '나라별' : '도시별'),
+            ],
+          ),
+          const Center(
+            child: Text('공지할 나라', style: TextStyle(fontSize: 20)),
+          ),
+          const SizedBox(height: 20),
+          _countries.isEmpty
+              ? const SizedBox()
+              : Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: country.cities!
-                      .map((city) => GestureDetector(
+                  children: _countries
+                      .map((country) => GestureDetector(
                             onTap: () {
-                              setState(() {
-                                if (_selectedCities.contains(city)) {
-                                  _selectedCities.remove(city);
-                                } else {
-                                  _selectedCities.add(city);
-                                }
-                              });
+                              onSelectedCountry(country);
                             },
                             child: Container(
                                 constraints: const BoxConstraints(
                                     minWidth: 80, minHeight: 80),
                                 decoration: BoxDecoration(
-                                    border: _selectedCities.contains(city)
+                                    border: _selectedCountries.contains(country)
                                         ? Border.all(
                                             color: Colors.green, width: 5)
                                         : Border.all(color: Colors.grey),
@@ -230,7 +173,11 @@ class _CreatePublicNoticeModalState extends State<CreatePublicNoticeModal> {
                                   crossAxisAlignment: CrossAxisAlignment.center,
                                   children: [
                                     Text(
-                                      city.name,
+                                      country.flagEmoji,
+                                      style: const TextStyle(fontSize: 20),
+                                    ),
+                                    Text(
+                                      country.name,
                                       style: const TextStyle(fontSize: 20),
                                     ),
                                   ],
@@ -238,107 +185,164 @@ class _CreatePublicNoticeModalState extends State<CreatePublicNoticeModal> {
                           ))
                       .toList(),
                 ),
-                const SizedBox(height: 20),
-              ],
+          const SizedBox(height: 20),
+          if (!nationalScope)
+            for (var country in _selectedCountries)
+              Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text('${country.name}의 도시',
+                      style: const TextStyle(fontSize: 20)),
+                  const SizedBox(height: 20),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: country.cities!
+                        .map((city) => GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  if (_selectedCities.contains(city)) {
+                                    _selectedCities.remove(city);
+                                  } else {
+                                    _selectedCities.add(city);
+                                  }
+                                });
+                              },
+                              child: Container(
+                                  constraints: const BoxConstraints(
+                                      minWidth: 80, minHeight: 80),
+                                  decoration: BoxDecoration(
+                                      border: _selectedCities.contains(city)
+                                          ? Border.all(
+                                              color: Colors.green, width: 5)
+                                          : Border.all(color: Colors.grey),
+                                      borderRadius: BorderRadius.circular(10)),
+                                  padding: const EdgeInsets.all(10),
+                                  margin: const EdgeInsets.only(right: 10),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        city.name,
+                                        style: const TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  )),
+                            ))
+                        .toList(),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+          const SizedBox(height: 20),
+          const Text('제목', style: TextStyle(fontSize: 20)),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _titleController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '제목을 입력하세요',
             ),
-        const SizedBox(height: 20),
-        const Text('제목', style: TextStyle(fontSize: 20)),
-        const SizedBox(height: 20),
-        TextField(
-          controller: _titleController,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: '제목을 입력하세요',
           ),
-        ),
-        const SizedBox(height: 20),
-        const Text('내용', style: TextStyle(fontSize: 20)),
-        const SizedBox(height: 20),
-        TextField(
-          controller: _contentController,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: '내용을 입력하세요',
+          const SizedBox(height: 20),
+          const Text('내용', style: TextStyle(fontSize: 20)),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _contentController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '내용을 입력하세요',
+            ),
           ),
-        ),
-        const SizedBox(height: 20),
-        const Text('순서', style: TextStyle(fontSize: 20)),
-        const SizedBox(height: 20),
-        TextField(
-          controller: _sortNumController,
-          onTap: () {
-            showModalBottomSheet(
-                context: context,
-                builder: (BuildContext context) {
-                  return Container(
-                    height: 200,
-                    child: CupertinoPicker(
-                      itemExtent: 40,
-                      onSelectedItemChanged: (int value) {
-                        _sortNumController.text = value.toString();
-                      },
-                      children: List<Widget>.generate(
-                        11,
-                        (int index) {
-                          return Center(
-                            child: Text(
-                              index.toString(),
-                              style: const TextStyle(fontSize: 20),
-                            ),
-                          );
+          const SizedBox(height: 20),
+          const Text('순서', style: TextStyle(fontSize: 20)),
+          const SizedBox(height: 20),
+          TextField(
+            controller: _sortNumController,
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                      height: 200,
+                      child: CupertinoPicker(
+                        itemExtent: 40,
+                        onSelectedItemChanged: (int value) {
+                          _sortNumController.text = value.toString();
                         },
+                        children: List<Widget>.generate(
+                          11,
+                          (int index) {
+                            return Center(
+                              child: Text(
+                                index.toString(),
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                            );
+                          },
+                        ),
                       ),
-                    ),
-                  );
-                });
-          },
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: '낮을 상위에 위치',
+                    );
+                  });
+            },
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '낮을 상위에 위치',
+            ),
           ),
-        ),
-        const SizedBox(height: 20),
-        MaterialButton(
-          minWidth: MediaQuery.of(context).size.width - 40,
-          height: 50,
-          color: Colors.blue,
-          onPressed: () {
-            /*publicNotice를 생성하고 확인하는 화면 표시 */
-            if (nationalScope) {
-              for (Country country in _selectedCountries) {
-                _createdPublicNotices.add(_createPublicNotice(
-                    _titleController.text,
-                    _contentController.text,
-                    int.parse(_sortNumController.text),
-                    nationalScope,
-                    null,
-                    country));
-                safePrint(_createdPublicNotices.toString());
+          const SizedBox(height: 20),
+          MaterialButton(
+            minWidth: MediaQuery.of(context).size.width - 40,
+            height: 50,
+            color: Colors.blue,
+            onPressed: () {
+              /*publicNotice를 생성하고 확인하는 화면 표시 */
+              if (nationalScope) {
+                generatePublicNoticeForCountries();
+              } else {
+                generatePublicNoticeForCities();
               }
-            } else {
-              for (City city in _selectedCities) {
-                _createdPublicNotices.add(_createPublicNotice(
-                    _titleController.text,
-                    _contentController.text,
-                    int.parse(_sortNumController.text),
-                    nationalScope,
-                    city,
-                    null));
-                safePrint(_createdPublicNotices.length.toString());
-              }
-            }
-          },
-          child: const Text('입력'),
-        ),
-      ],
+            },
+            child: const Text('입력'),
+          ),
+        ],
+      ),
     );
+  }
+
+  void generatePublicNoticeForCities() {
+    for (City city in _selectedCities) {
+      _createdPublicNotices.add(_createPublicNotice(
+          _titleController.text,
+          _contentController.text,
+          int.parse(_sortNumController.text),
+          nationalScope,
+          city,
+          null));
+      safePrint(_createdPublicNotices.toString());
+    }
+  }
+
+  void generatePublicNoticeForCountries() {
+    for (Country country in _selectedCountries) {
+      _createdPublicNotices.add(_createPublicNotice(
+          _titleController.text,
+          _contentController.text,
+          int.parse(_sortNumController.text),
+          nationalScope,
+          null,
+          country));
+      safePrint(_createdPublicNotices.toString());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _createAppBar(),
-      body: _createBody(),
+      body: SingleChildScrollView(child: _createBody()),
     );
   }
 }
